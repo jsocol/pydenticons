05d067875ddd0681bfcdea2d749bbc8edab76b81
James Socol <james@mozilla.com>
2010-05-05 15:35:07 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pydenticons',
+    version='0.1',
+    description='Identicons library in Python from Shin Adachi',
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='http://github.com/jsocol/identicon',
+    license='BSD',
+    packages=['pydenticons'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
